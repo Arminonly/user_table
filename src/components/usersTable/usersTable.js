3b0354281c68f365b3b004ee4b28e0d42764f0ec
Arminonly <amiaby2016@gmail.com>
2022-02-12 16:23:05 +0300

    first commit

--- src/components/usersTable/usersTable.js
+++ src/components/usersTable/usersTable.js
@@ -1,12 +1,11 @@
 import React from 'react'
 import { Row, Col } from 'antd'
-import  UserTable  from './Table'
+import UserTable from './Table'
 
 export default function UsersTable() {
   return (
     <div>
       <Row>
-      
         <Col xs={24} md={{ span: 20, offset: 2 }}>
           <UserTable />
         </Col>

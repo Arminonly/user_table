855808e48b1b0c1124c379921efec897c63ba9b7
Arminonly <amiaby2016@gmail.com>
2022-02-13 21:52:36 +0300

    deleted const columns

--- src/components/usersTable/Table.js
+++ src/components/usersTable/Table.js
@@ -7,88 +7,6 @@ import { Link } from 'react-router-dom'
 import Highlighter from 'react-highlight-words'
 import { SearchOutlined } from '@ant-design/icons'
 
-// const columns = [
-//   {
-//     title: 'Имя Фамилия',
-//     dataIndex: 'name',
-//     key: 'name',
-//     render: (text) => (
-//       <a href="https://web.telegram.org/k/" target="_blank">
-//         {text}
-//       </a>
-//     )
-//   },
-//   {
-//     title: 'Telegram',
-//     dataIndex: 'telegram',
-//     key: 'telegram'
-//   },
-//   {
-//     title: 'Instagram',
-//     dataIndex: 'instagram',
-//     key: 'instagram'
-//   },
-//   {
-//     title: 'Логин',
-//     dataIndex: 'login',
-//     key: 'login'
-//   },
-//   {
-//     title: 'Название модуля',
-//     dataIndex: 'tags',
-//     key: 'tags',
-//     render: (tags) => (
-//       <>
-//         {tags.map((tag) => (
-//           <Tag color="geekblue" key={tag}>
-//             {tag}
-//           </Tag>
-//         ))}
-//       </>
-//     ),
-//     filters: [
-//       {
-//         text: 'HTML/CSS',
-//         value: 'HTML/CSS'
-//       },
-//       {
-//         text: 'Базы данных',
-//         value: 'Базы данных'
-//       },
-//       {
-//         text: 'JS',
-//         value: 'JS'
-//       },
-//       {
-//         text: 'Node',
-//         value: 'Node'
-//       },
-//       {
-//         text: 'React',
-//         value: 'React'
-//       }
-//     ],
-//     onFilter: (value, item) => item.tags.includes(value)
-//   },
-//   {
-//     title: 'Дата старта',
-//     dataIndex: 'startDate',
-//     key: 'startDate',
-//     sorter: (a, b) => a.startDate - b.startDate
-//   },
-//   {
-//     title: 'Действие',
-//     dataIndex: 'action',
-//     key: 'action',
-//     render: (text) => (
-//       <Space size="middle">
-//         <Link to="/ProgressMain">Прогресс</Link>
-//         <a>Изменить </a>
-//       </Space>
-//     )
-//   }
-// ]
-
 export default class UserTable extends React.Component {
   // const dataSource = users.map((item) => ({ ...item, key: item.id }))
 
@@ -311,9 +229,7 @@ export default class UserTable extends React.Component {
             onOk={() => this.modalIsVisible(false)}
             onCancel={() => this.modalIsVisible(false)}
           >
-            <Input
-            
-            />
+            <Input/>
           </Modal>
         </p>
       </>
